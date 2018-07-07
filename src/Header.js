6978a4f45dfccfaf00a7aa4189ad45043fdc4de5
Tom Rodrigues <tomjrodrigues@gmail.com>
2018-07-07 11:59:22 -0700

    added Header.js

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	// event handlers
+
+	render() {
+		let title = "Podcast Faces"
+		return(
+			<div>
+				<h1>{title}</h1>
+				<h6>By Tom Rodrigues: <a href="https://github.com/TomJRodrigues">GitHub</a>, <a href="mailto:tomjrodrigues@gmail.com">tomjrodrigues@gmail.com</a></h6>
+			</div>
+		)
+	}
+
+}
+
+
+
+	
