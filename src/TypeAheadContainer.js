55bcaf2635ed253737ba53ba3d74cc61bc593398
Tom Rodrigues <tomjrodrigues@gmail.com>
2018-10-23 11:29:29 -0700

    better styling

--- src/TypeAheadContainer.js
+++ src/TypeAheadContainer.js
@@ -20,30 +20,34 @@ export default class TypeAheadContainer extends Component {
 	render() {
 		if (this.props.state.showByShow === true) {
 			return(
-				<Typeahead
-					onChange={this.handleChange}
-	        labelKey={(option) => `${option.show}`}
-	        options={this.props.state.typeAheadOptions}
-	        placeholder="Enter a show"
-	        highlightOnlyResult={true}
-	        selectHintOnEnter={true}
-	        clearButton
-	        selected={this.props.state.selected}
-	      />
+				<div className="typeahead-container">
+					<Typeahead
+						onChange={this.handleChange}
+		        labelKey={(option) => `${option.show}`}
+		        options={this.props.state.typeAheadOptions}
+		        placeholder="Enter a show"
+		        highlightOnlyResult={true}
+		        selectHintOnEnter={true}
+		        clearButton
+		        selected={this.props.state.selected}
+		      />
+	      </div>
 			)
 		}
 		if (this.props.state.showByName === true) {
 			return(
-				<Typeahead
-					onChange={this.handleChange}
-	        labelKey={(option) => `${option.firstName} ${option.lastName}`}
-	        options={this.props.state.typeAheadOptions}
-	        placeholder="Enter a name"
-	        highlightOnlyResult={true}
-	        selectHintOnEnter={true}
-	        clearButton
-	        selected={this.props.state.selected}
-	      />
+				<div className="typeahead-container">
+					<Typeahead
+						onChange={this.handleChange}
+		        labelKey={(option) => `${option.firstName} ${option.lastName}`}
+		        options={this.props.state.typeAheadOptions}
+		        placeholder="Enter a name"
+		        highlightOnlyResult={true}
+		        selectHintOnEnter={true}
+		        clearButton
+		        selected={this.props.state.selected}
+		      />
+		    </div>
 			)
 		}
 		else{
